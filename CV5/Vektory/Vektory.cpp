b2c82362f0df939e6215bbcd4fa941f0fea8f675
GitTimotej <timotej.hertl@gmail.com>
2025-10-23 13:27:28 +0200

    prvy commit

---
+++ CV5/Vektory/Vektory.cpp
@@ -0,0 +1,41 @@
+﻿// Vektory.cpp : Defines the entry point for the application.
+//
+
+#include "Vektory.h"
+#include "math.h"
+
+using namespace std;
+void soucet(double a[3], double b[3], double c[3]) {
+	c[0] = a[0] + b[0];
+	c[1] = a[1] + b[1];
+	c[2] = a[2] + b[2];
+}
+double velikost(double a[3]) {
+	return sqrt(a[0]*a[0]+a[1]*a[1]+a[2]*a[2]);
+}
+double skalarniSoucin(double a[3], double b[3]) {
+	return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+}
+//double vektorovySoucin(double a[3], double b[3]) { 
+	//return (a[1] * b[2]) - (a[2] * b[1]), (a[2] * b[0]) - (a[0] * b[1]), (a[0] * b[1]) - (a[1] * b[0]);
+void vektorovySoucin(double a[3], double b[3], double c[3]) {
+	c[0] = (a[1] * b[2]) - (a[2] * b[1]);
+	c[1] = (a[2] * b[0]) - (a[0] * b[2]);
+	c[2] = (a[0] * b[1]) - (a[1] * b[0]);
+	}
+int main()
+{
+	double a[3], b[3], c[3];
+	printf("Zadajte  suradnice vektoru A oddelene ciarkami");
+	scanf_s("%lf,%lf,%lf", &a[0], &a[1], &a[2]);
+	printf("Zadajte  suradnice vektoru B oddelene ciarkami");
+	scanf_s("%lf,%lf,%lf", &b[0], &b[1], &b[2]);
+	printf("\n");
+	soucet(a, b, c);
+	printf("Vysledok suctu vektoru A a B je: (%lf,%lf,%lf)\n", c[0], c[1], c[2]);
+	printf("Velkost vektoru A je: %lf\n",velikost(a));
+	printf("Skalarni sucin vektorov A a B je: %lf\n", skalarniSoucin(a, b));
+	vektorovySoucin(a, b, c);
+	printf("Vektorovy sucin vektorov A a B je: (%lf,%lf,%lf)\n", c[0],c[1],c[2]);
+	return 0;
+}
---
+++ CV6/PrepisTextu/PrepisTextu.cpp
@@ -0,0 +1,51 @@
+﻿// PrepisTextu.cpp : Defines the entry point for the application.
+//
+
+#include "PrepisTextu.h"
+#include <ctype.h>
+
+using namespace std;
+
+int pocetPismen(char* text) {
+	int pocet = 0;
+	char* znak;
+	for (znak = text; *znak != 0; ++znak) {
+		if (isalpha(*znak)) {
+			pocet++;
+		}
+	}
+
+	return pocet;
+}
+int pocetCisel(char* text) {
+	int pocet = 0;
+	char* znak;
+	for (znak = text; *znak != 0; ++znak) {
+		if (isdigit(*znak)) {
+			pocet++;
+		}
+	}
+
+	return pocet;
+}
+int pocetSlov(char* text) {
+	int pocet = 0;
+	int prechZnak = 0;
+	char* znak;
+	for (znak = text; *znak != 0; ++znak) {
+		if (isspace(*znak) && !isspace(prechZnak)) {
+			pocet++;
+		}
+		prechZnak = *znak;
+	}
+	
+	return pocet;
+}
+int main()
+{
+	char text[] = "Toto jsou 2 vzorove vety, ktere budou zpracovavany Vasemi       funkcemi. Auto Hyundai ix35 ma pres 120 konskych sil.";
+	printf("Pocet pismen v textu je: %d\n", pocetPismen(text));
+	printf("Pocet cisel v textu je: %d\n", pocetCisel(text));
+	printf("Pocet slov v textu je: %d\n", pocetSlov(text));
+	return 0;
+}
